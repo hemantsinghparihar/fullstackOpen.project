862de8a07ce0e231a529474b2d14a86d76189823
hemant <hemantsinghparihar13@gmail.com>
2024-01-25 11:47:32 +0530

    adding the 2.6 version of phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,34 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('');
+
+  const handleAddPerson=(event)=>{
+    event.preventDefault(); //this prevent the default behaviour of form submission which is reloading the page automatically when the form is submitted
+     const newPerson={
+      name:newName,
+     }
+     setPersons(persons.concat(newPerson));
+console.log('✌️persons.concat(newPerson) --->', persons.concat(newPerson));
+     setNewName("")
+     
+
+  }
+  const handleNewName=(event)=>{
+     
+    console.log('✌️event.target.value() --->', event.target.value);//in the event.target.value we receive the inputs that the user is typing on every time he enters a letter we recieve that letter
+    setNewName(event.target.value)
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddPerson}>
+        <div>
+          name: <input type='text' value={newName} onChange={handleNewName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person=>
+        <p>{person.name}</p>
+        )}
+      {/* <p>{persons[0].name}</p> */}
+       
+       
+    </div>
   )
 }
 
