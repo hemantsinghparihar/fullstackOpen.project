6dd72629c03c800ae60c8345a5f6bcca65bc52f4
hemant <hemantsinghparihar13@gmail.com>
2024-01-26 21:18:24 +0530

    phonebook version 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',number:7417111462 },
+    { name: 'Arto Hellas',number:7417111462 },{ name: 'hemu singh',number:74172222 },{ name: 'hemant parihar',number:74172222 }
   ]) 
   const [newName, setNewName] = useState('');
   const [newNum, setNewNum] = useState('');
+  const [filter, setfilter] = useState('');
+  const [filteredUsers, setfilterUser] = useState([]);
 
   // ----------------handling addition of the new inputs---------------------
 
@@ -54,11 +56,35 @@ const App = () => {
     console.log('✌️event.target.value() --->', event.target.value);//in the event.target.value we receive the inputs that the user is typing on every time he enters a letter we recieve that letter
     setNewNum(event.target.value)
   }
+  const hadleFiltering=(event)=>{
+    const filter=event.target.value.toLowerCase();
+
+    setfilter(filter)
+    //here we will be finding the person by comparing the input with existing people
+     const filtered = persons.filter(user =>
+      user.name.toLowerCase().includes(filter)
+    );
+    setfilterUser(filtered)
+
+  }
+  //processing part  --------------
+  // so i have to create a filter feature which will be done using an input field when user starts entering the name we have to take each letter of input and match it with our name which existing and those name are matching should be displayed there
 
 // rendering part-----------------------------------------------
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        filter by name: <input type="text" value={filter} onChange={hadleFiltering}/>
+        <ul>
+        {filteredUsers.map(user => (
+          <li key={user.name}>
+            {user.name} - {user.number}
+          </li>
+        ))}
+      </ul>
+      </div>
+      <h2>Add A New Person</h2>
       <form onSubmit={handleAddPerson}>
         <div>
           name: <input type='text' value={newName} onChange={handleNewName}/>
