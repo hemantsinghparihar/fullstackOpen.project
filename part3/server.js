011ea0f327c0bdc769db5c4e3f70a864bab904be
hemant <hemantsinghparihar13@gmail.com>
2024-02-02 15:41:03 +0530

    part3 sec a completed

--- part3/server.js
+++ part3/server.js
@@ -1,8 +1,24 @@
 const express=require("express");
+const morgan=require("morgan");
 const app=express();
 const PORT=3001;
 
 app.use(express.json());
+
+//app.use(morgan('dev'));
+
+morgan.token('post-data', (req) => {
+  return JSON.stringify(req.body);
+  // if (req.method === 'POST') {
+  //   return JSON.stringify(req.body);
+  // }
+  // return '-';
+});
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :post-data')
+);
+ 
 
 const phonebook=[
     { 
